b1c30995c999b7612b1a47ce0a3722ca23c3b289
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-06-24 23:22:50 +0200

    Update Recursion10.c

--- Recursion10.c
+++ Recursion10.c
@@ -1,10 +1,8 @@
 /*
 Si scriva un programma C che definisce le funzioni:
- leggi(), che legge da tastiera un numero intero non negativo
-e lo restituisce. Se fosse negativo, deve continuare a chiederlo
-all'utente;
+ leggi(), che legge da tastiera un numero intero non negativo e lo restituisce. Se fosse negativo, deve continuare a chiederlo all'utente;
  stampa(int numero), che stampa le cifre del numero indicato, in italiano. Per esempio, se numero e 4301 allora deve stampare quattro tre zero uno; se numero e 0 allora deve stampare zero.
- È possibile denire ulteriori funzioni ausiliarie, se servono. La funzione stampa()deve essere ricorsiva o chiamare una vostra funzione ricorsiva.
+ È possibile denire ulteriori funzioni ausiliarie, se servono. La funzione stampa() deve essere ricorsiva o chiamare una vostra funzione ricorsiva.
  Caso base: numero = 0;
  Passo ricorsivo: stampa( numero/10 );
  Infine, tale programma deve avere anche un main()che
@@ -23,6 +21,7 @@ int leggi() {
 
     return num;
 }
+
 void stampa(int num) {
     int joker = num % 10;
     int result;
