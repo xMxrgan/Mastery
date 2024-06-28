594ba33c261792ca1e6d0e9f3592d6a9f3e1314d
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-06-28 19:01:39 +0200

    Update LinkedList06.c
    Missing the recursive inserction

--- Linked Lists/LinkedList06.c
+++ Linked Lists/LinkedList06.c
@@ -7,3 +7,65 @@ Si modifichi il file ESA 15062017 B 1.c tale che:
 Inserire una stringa: c0a09se3t367t8a Creazione della lista...
 La lista è: c -> a -> s -> e -> t -> t -> a
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DIM 256
+
+typedef struct node {
+    char data;
+    struct node *next;
+} elem;
+
+char *read();
+void printList(elem *);
+elem *createList(elem *, char);
+
+int main() {
+    char *String;
+    int Joker;
+    elem *List = NULL;
+
+    String = read();
+
+    for (Joker = 0; Joker != '\0'; Joker ++) {
+
+        if (String[Joker] < '0' && String[Joker] > '9') {
+
+            List = createList(List, String[Joker]);
+            printList(List);
+        }
+    }
+}
+
+char *read() {
+    char *string;
+
+    printf("Give me your string!\nBut it has to be shorter than 256 characters.\n");
+    scanf("%s", string);
+
+    while (strlen(string) < DIM) {
+        printf("It has to be shorter than 256 characters.\n");
+        scanf("%s", string);
+    }
+
+    return string;
+}
+
+elem *createList(elem *list, char string) {
+
+}
+
+void printList(elem *list) {
+    
+    printf("%c -> ", list -> data);
+
+    while (list -> next != NULL) {
+
+        list = list -> next;
+        printf("%d", list -> data);
+        
+    }
+}
