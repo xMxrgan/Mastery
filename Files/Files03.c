55c8d4005f601c8c1d25c49e045e38b6ea481071
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-06-29 20:05:01 +0200

    Add files via upload

---
+++ Files/Files03.c
@@ -0,0 +1,3 @@
+/*
+Scrivere un programma C che chiede all’utente una sequenza di numeri a priori illimitata e che termina con l’inserimento del numero 0 – non fa parte della sequenza. Il programma deve salvare i numeri (0 escluso) in un file di nome valori.txt e come ultimo valore deve scrivere nel file quanti numeri sono stati salvati. Ad esempio se l’utente inserisce 5 2 4 0 nel file nel file valori.txt troveremo 5 2 4 3.
+*/
---
+++ Files/Files08.c
@@ -0,0 +1,8 @@
+/*
+Scrivere un programma C che apre un file di testo “testo.txt” contenente parole ciascuna di al massimo 15 caratteri. Il programma identifica e stampa a video tutte le parole che rappresentano un numero (cioè composte soltanto da cifre). Ad esempio, se il file “input3.txt” contiene il testo:
+oggi è il 30 marzo 2020 e per superare l’esame è necessario prendere almeno 18 nella prova1 di programmazione
+Il programma stamperà a video: 30
+2020
+18
+Variante: stampare le parole che contengono almeno 3 vocali minuscole
+*/
