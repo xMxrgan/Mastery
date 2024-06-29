0f5026ad78a6722bad4de29dd83065707d12ae9c
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-06-29 15:16:54 +0200

    Update LinkedListHead.c

--- Linked Lists/LinkedListHead.c
+++ Linked Lists/LinkedListHead.c
@@ -11,9 +11,32 @@ void view(elem *);
 void maximum(elem *, int *, int *);                    // Passed through pointer
 
 int main() {
+
     elem *List = NULL;
+    int Num;
+    int Max, Min;
     int Result;
-    
+
+    scanf("%d", &Num);
+
+    while(Num < 0){
+
+        List = insertHead(elem *List, int Num);
+        view(elem *List);
+        scanf("%d", &Num);
+    }
+
+
+elem *insertHead(elem *List, int num) {
+    elem *tmp;
+
+    tmp = (elem *)malloc(sizeof(elem));
+
+    if (tmp == NULL) {
+        
+
     
     
 }
+
+    
