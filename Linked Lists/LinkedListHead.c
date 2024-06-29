6a00937f09cf06a0fb9dff4d1ce6222c4b4fd033
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-06-29 12:17:35 +0200

    Update LinkedListHead.c

---
+++ Linked Lists/LinkedListHead.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node *next;
+} elem;
+
+elem *insertHead(elem *, int);
+void view(elem *);
+void maximum(elem *, int *, int *);
+
+int main() {
+    
+}
