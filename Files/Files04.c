80e86225fd91b2d35c1adb9bdd03ae86673cb6e1
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-06-30 16:10:15 +0200

    Update Files04.c

--- Files/Files04.c
+++ Files/Files04.c
@@ -2,7 +2,7 @@
 Scrivere un programma C che crea il file di testo “tabellina.txt” con la tabellina pitagorica fino a un dato numero (per es. fino a 5). Utilizza il formato "%3d\t" per allineare i numeri a destra con almeno 3 spazi come dimensione minima.
 */
 
-#include <stdio.c>
+#include <stdio.h>
 
 int main() {
     int Num, Joker, Jolly;
