d4da708ee0c987e1a4494eeb8d6939afaf92fb3c
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-06-30 16:09:44 +0200

    Update Files04.c

--- Files/Files04.c
+++ Files/Files04.c
@@ -1,3 +1,22 @@
 /*
 Scrivere un programma C che crea il file di testo “tabellina.txt” con la tabellina pitagorica fino a un dato numero (per es. fino a 5). Utilizza il formato "%3d\t" per allineare i numeri a destra con almeno 3 spazi come dimensione minima.
 */
+
+#include <stdio.c>
+
+int main() {
+    int Num, Joker, Jolly;
+    FILE *file;
+
+    file = fopen("tabellina.txt", "w");
+
+    printf("Please, give me the cap for the multipliers:\n");
+    scanf("%d", &Num);
+
+    for (Joker = 1; Joker == Num; Joker ++) {
+        for (Jolly = 1; Jolly == 10; Jolly ++) {
+            
+            printf("%3d\t", Jolly * Joker);
+        }
+    }
+}
