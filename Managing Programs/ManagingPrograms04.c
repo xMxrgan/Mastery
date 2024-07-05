2fc90629bf957d5c154898cd21618c30da7ec0c2
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-07-05 15:34:24 +0200

    Update ManagingPrograms04.c

--- Managing Programs/ManagingPrograms04.c
+++ Managing Programs/ManagingPrograms04.c
@@ -19,3 +19,69 @@ La funzione main() produce il seguente output:
 3 -> 6 -> 10 -> 2 -> 8
 8 -> 3 -> 6 -> 10 -> 2
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int datum;
+    struct node *next;
+} elem;
+
+elem *createList(elem *, int);
+void printList(elem *);
+//void destroy(elem *);
+// elem *leftRotate(elem *);
+// elem *rightRotate(elem *);
+
+int main() {
+    elem *List = NULL;
+    int Datum;
+
+    printf("Give me a sequence of positive numbers for the list, negative for stopping the scan:\n");
+
+    do {
+        scanf("%d", &Datum);
+        List = createList(List, Datum);
+    } while (Datum > 0);
+
+    printList(List);
+}
+
+elem *createList(elem *list, int datum) {
+    elem *tmp;
+    elem *prec;
+
+    tmp = (elem *)malloc(sizeof(elem));
+
+    if (tmp != NULL) {
+        tmp -> datum = datum;
+        tmp -> next = NULL;
+
+        if (list == NULL) {
+            list = tmp;
+
+        } else {
+
+            for (prec = list; prec -> next != NULL; prec = prec -> next) {
+                prec -> next = tmp;
+            }
+        }
+
+    } else {
+        printf("Error!");
+    }
+
+    free(tmp);
+
+    return list;
+}
+
+void printList(elem *list) {
+
+    printf("Your original list is:\n");
+    while (list != NULL) {
+        printf("%d ", list -> datum);
+        list = list -> next;
+    }
+}
