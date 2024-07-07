1e667fd2b4fb76a147dde3308e27ab323045c9ec
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-07-07 11:49:07 +0200

    Update ManagingPrograms04.c

--- Managing Programs/ManagingPrograms04.c
+++ Managing Programs/ManagingPrograms04.c
@@ -30,58 +30,110 @@ typedef struct node {
 
 elem *createList(elem *, int);
 void printList(elem *);
-//void destroy(elem *);
-// elem *leftRotate(elem *);
-// elem *rightRotate(elem *);
+void destroy(elem *);
+elem *leftRotate(elem *);
+elem *rightRotate(elem *);
 
 int main() {
     elem *List = NULL;
-    int Datum;
+    int Datum, Choice;
 
     printf("Give me a sequence of positive numbers for the list, negative for stopping the scan:\n");
 
-    do {
-        scanf("%d", &Datum);
+    scanf("%d", &Datum);
+    while (Datum > 0) {
         List = createList(List, Datum);
-    } while (Datum > 0);
+        scanf("%d", &Datum);
+    }
 
     printList(List);
+
+    printf("Write number '1' for destroying the list, number '2' for rotating it on the left and '3' for rotating it on the right:\n");
+    scanf("%d", &Choice);
+
+    if (Choice == 1) {
+        destroy(List);
+        printf("Your list is terminated!\nI'll be back");
+    } else if (Choice == 2) {
+        List = leftRotate(List);
+        printList(List);
+
+    } else if (Choice == 3) {
+        List = rightRotate(List);
+        printList(List);
+
+    } else {
+        printf("Your response is invalide, closing the program.");
+    }
 }
 
-elem *createList(elem *list, int datum) {
+elem *createList(elem *list, int num) {
     elem *tmp;
     elem *prec;
 
     tmp = (elem *)malloc(sizeof(elem));
 
     if (tmp != NULL) {
-        tmp -> datum = datum;
+        tmp -> datum = num;
         tmp -> next = NULL;
 
         if (list == NULL) {
             list = tmp;
-
         } else {
-
-            for (prec = list; prec -> next != NULL; prec = prec -> next) {
-                prec -> next = tmp;
-            }
+            for (prec = list; prec -> next != NULL; prec = prec -> next);
+            prec -> next = tmp;
         }
 
     } else {
         printf("Error!");
     }
-
-    free(tmp);
-
     return list;
 }
 
 void printList(elem *list) {
 
-    printf("Your original list is:\n");
+    printf("Your list is:\n");
     while (list != NULL) {
         printf("%d ", list -> datum);
         list = list -> next;
     }
+    printf("\n");
+}
+
+void destroy(elem *list) {
+    elem *tmp;
+
+    while (list != NULL) {
+        tmp = list;
+        list = tmp -> next;
+        free(tmp);
+    }
+}
+
+elem *leftRotate(elem *list) {
+    elem *tmp = list;
+    elem *prec;
+
+    for (prec = list; prec -> next != NULL; prec = prec -> next);
+    prec -> next = tmp;
+
+    tmp -> next = NULL;
+
+    list = list -> next;
+
+    return list;
+}
+
+elem *rightRotate(elem *list) {
+    elem *tmp;
+    elem *prec;
+
+    for (prec = list; prec -> next -> next != NULL; prec = prec -> next);
+    tmp = prec -> next; // Ultimo nodo
+    prec -> next = NULL;
+    tmp -> next = list;
+
+    list = tmp;
+
+    return list;
 }
