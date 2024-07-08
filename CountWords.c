b4dbb6c5406eb23091284bf4561e5a7663efe4da
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-07-08 16:59:18 +0200

    Update CountWords.c

--- CountWords.c
+++ CountWords.c
@@ -8,13 +8,13 @@ Put a control that the words have to be at least of 4 letter
 #include <string.h>
 
 typedef struct node {
-    int word;
-    int times;
+    char *word;
+//    int times;
     struct node *next;
 } elem;
 
-elem *createList(elem *, char *, int);
-//void write();
+elem *createList(elem *, char *);
+void write(elem *);
 //int counter(char *);
 //elem *alphabetOrder(elem *);
 //elem *freqOrder(elem *);
@@ -22,12 +22,64 @@ elem *createList(elem *, char *, int);
 int main() {
     FILE *File;
     char *String;
-    elem *List;
+    elem *List = NULL;
+//    int Count;
+//
+    String = (char *)malloc(sizeof(char));
 
-    File = fopen("CounterText.txt", "r");
+    if (File == NULL) {
+        printf("Error!");
+    } else {
 
-    while (!feof(File)) {
-        fscanf(File, "%s ", String);
-        List = createList(List, String, )
+        File = fopen("CounterRead.txt", "r");
+
+        while (!feof(File)) {
+
+            fscanf(File, "%s ", String);
+            List = createList(List, String);
+        }
+        write(List);
     }
+
+    fclose (File);
+    free(String);
+}
+
+elem *createList(elem *list, char *string) {
+    elem *tmp;
+    elem *prec;
+
+    tmp = (elem *)malloc(sizeof(elem));
+
+    if (tmp != NULL) {
+        tmp -> word = string;
+        tmp -> next = NULL;
+
+        if (list == NULL) {
+            list = tmp;
+
+        } else {
+            for (prec = list; prec -> next != NULL; prec = prec -> next);
+            prec -> next = tmp;
+        }
+
+    } else {
+        printf("Error!");
+    }
+
+    return list;
+}
+
+void write(elem *list) {
+    FILE  *fileW;
+    elem *tmp = list;
+
+    fileW = fopen("CounterWrite.txt", "w");
+
+    while (tmp != NULL) {
+        printf("%s\t\n", tmp -> word);
+        tmp = tmp -> next;
+    }
+
+    fclose(fileW);
 }
