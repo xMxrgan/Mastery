dd96eb6beb8a08d48dc8496a02c11d5d161a3c10
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-07-13 15:34:19 +0200

    Update Matrixes01.c

--- Matrixes/Matrixes01.c
+++ Matrixes/Matrixes01.c
@@ -1,6 +1,23 @@
 /*
 Si scriva un programma che copi tutti i valori di
-una matrice int M[D1][D2] in un array di
+una matrice int M[D1][D2] in un array (// Io agggiungo dinamico) di
 dimensione D1*D2 eliminando nel processo di
 copia tutti i duplicati negativi.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define D1 5
+#define D2 4
+
+typedef struct node {
+    struct node *next;
+    int datum;
+} elem;
+
+elem *createList(elem *, int);
+
+int main() {
+    int Matrix[D1][D2];
+    
+}
