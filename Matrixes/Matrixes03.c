cc94683c507e2f90c050f8aa1adb44e1c2289410
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-07-14 10:43:43 +0200

    Update Matrixes03.c

--- Matrixes/Matrixes03.c
+++ Matrixes/Matrixes03.c
@@ -8,16 +8,30 @@ Scrivere un programma che data una matrice int A[M][M] verifichi se per ogni ele
 int main() {
     int A[M][M];
     int Num, J, I;
+    int X, Y;
+    int Joker = 1;                                        // Flag
 
     for (J = 0; J < M; J ++) {
         for (I = 0; I < M; I ++) {
-
             scanf("%d", &A[J][I]);
-            
         }
     }
 
+    for (X = 0; X < M; X ++) {
+        for (Y = M - 1; Y >= 0; Y --) {
+            
+            if (A[X][X] < A[X][Y]) {
+                Joker = 1;
+                break;
+            }
+        }
+    }
 
-
+    if (Joker == 1) {
+        printf("For each element of the main diagonal there is a bigger element in the secondary diagonal");
+    } else {
+        printf("For each element of the main diagonal there is NOT a bigger element in the secondary diagonal");
+    }
     
 }
+
