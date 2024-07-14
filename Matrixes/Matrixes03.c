5bf5c7566601b7661b1eb8e9f8c49858d34a7c34
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-07-14 10:06:58 +0200

    Update Matrixes03.c

--- Matrixes/Matrixes03.c
+++ Matrixes/Matrixes03.c
@@ -1,6 +1,23 @@
 /*
-Scrivere un programma che data una matrice int
-A[M][M] verifichi se per ogni elemento x della
-diagonale principale esiste un elemento y della
-diagonale secondaria tale che y>x.
+Scrivere un programma che data una matrice int A[M][M] verifichi se per ogni elemento x della diagonale principale esiste un elemento y della diagonale secondaria tale che y>x.
 */
+
+#include <stdio.h>
+#define M 3
+
+int main() {
+    int A[M][M];
+    int Num, J, I;
+
+    for (J = 0; J < M; J ++) {
+        for (I = 0; I < M; I ++) {
+
+            scanf("%d", &A[J][I]);
+            
+        }
+    }
+
+
+
+    
+}
