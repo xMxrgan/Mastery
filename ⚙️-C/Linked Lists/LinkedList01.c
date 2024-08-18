a84e0ee73ce654afa6469fac5e0b31c00fcd627e
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-08-18 11:05:16 +0200

    Delete ⚙️-C directory

--- ⚙️-C/Linked Lists/LinkedList01.c
+++
@@ -1,8 +0,0 @@
-/*
-Sia data una lista i cui nodi sono definiti tramite la seguente struttura C:
-struct nodo{
-int valore;
-struct nodo *next; };
-Scrivere due funzioni C RICORSIVE che ricevendo in ingresso un puntatore alla lista: (1) stampano la lista e (2) contano quanti multipli di 3 sono presenti nella lista.
-NB: per testare il programma scrivere la funzione inserisci in testa per creare una lista con 10 valori.
-*/
--- ⚙️-C/Recursion/Recursion2.c
+++
@@ -1,9 +0,0 @@
-/*
-Si scrivano le versioni ricorsiva ed iterativa (utilizzo
-di while) di una funzione double f(double a, int n) che
-calcola il seguente valore: σ𝑖=1 𝑛 (𝑎 −𝑖/𝑎)
-*/
-
-int main() {
-
-}
