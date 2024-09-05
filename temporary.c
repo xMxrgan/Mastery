53a8fa68fb0e216cbf12664e14cf61e9abe7212a
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-09-05 16:17:31 +0200

    Create temporary.c

---
+++ temporary.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node *next;
+} elem;
+
+elem *CreateList(elem *list, int num) {
+    elem *tmp = NULL;
+
+    tmp = (elem *)malloc(sizeof(elem));
+
+    if (tmp) {
+        tmp -> data = num;
+        tmp -> next = NULL;
+
+        if (list) {
+            tmp -> next = list;
+            list = tmp;
+
+        } else {
+            list = tmp;
+        }
+
+    } else {
+        printf("Error!");
+    }
+
+    return list;
+}
+
+void viewList(elem *list) {
+    
+}
+
+
+int main() {
+    elem *List = NULL;
+    int Num;
+
+    scanf("%d", &Num);
+    while (Num) {
+        List = CreateList(List, Num);
+        scanf("%d", &Num);
+    }
+
+
+}
