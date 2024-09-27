ef4405a838c8f000ba4de8f8acb7f008d994960f
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-09-27 09:43:16 +0200

    Update and rename IfElse to IfElse.java

---
+++ Java ☕️/Conditionals/IfElse.java
@@ -0,0 +1,41 @@
+/*
+Scrivere un programma Java che chiede in ingresso due numeri a e b e verifica se a
+e' multiplo di b, oppure b è multiplo di a e stampa un opportuno messaggio a video
+
+Scrivere un programma che chiede all'utente due caratteri e verifica se sono uno il
+successivo dell'altro (ad esempio i caratteri 'a' e 'b' sono uno il successivo dell'altro)
+
+Si scriva un programma, in linguaggio Java, che legga da input le lunghezze dei lati
+di un triangolo e determini se il triangolo è equilatero, isoscele o scaleno
+*/
+
+import java.util.Scanner;
+
+  public class Main {
+    public static void main (String[] args) {
+      int A, B;
+
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.println("Hello, please tell me your name:");
+      String Name = scanner.nextLine();
+
+      System.out.println("Nice to meet you " + Name);
+
+      System.out.println("Now please, give me 2 numbers, il tell you which one is multiple of the other:");
+
+      A = scanner.nextInt();
+      scanner.nextLine();
+      B = scanner.nextInt();
+
+      System.out.println("You gave me " + A + " and " + B);
+
+      if (A % B == 0) {
+        System.out.println(A + " is multiple of " + B);
+      } else if (B % A == 0 ){
+        System.out.println(B + " is multiple of " + A);
+      } else {
+        System.out.println("They aren't multipliers!");
+      }
+    }
+  }
