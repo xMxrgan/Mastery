83c82c9b19dd716c8f141b6ba29891e02ecae8b8
Morgan <104979421+0xMxrgan@users.noreply.github.com>
2024-09-28 16:22:09 +0200

    Update PythagoricTable.java

--- Java ☕️/PythagoricTable.java
+++ Java ☕️/PythagoricTable.java
@@ -1,14 +1,6 @@
 /*
 Esercizi con i cicli:
 - Scrivere un programma che stampa la tavola pitagorica di un numero
-- Dato un numero intero n >0 letto da tastiera, stampare a video quante cifre pari
-con<ene. Esempio, il numero 8 ha 1 cifra pari, il numero 256 ha 2 cifre pari.
-- Da< in input due numeri interi n e y entrambi > 0, stampare a video quante cifre di n
-sono divisori di y. Esempio se n=156 e y=48 l’output sarà 2 (le cifre 1 e 6 di n sono
-divisori di 48).
-- Data una sequenza di numeri interi che termina con lo 0 (sentinella), stampare a
-video quan< mul<pli di 5 o di 7 sono contenuti nella sequenza (a:enzione, se un
-numero è divisore sia di 5 che di 7 non deve essere contato 2 volte).
 */
 
 import java.util.Scanner;
