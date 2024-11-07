9ae49138251ae75945f9a785ae2b5fd006227b3f
Morgan <104979421+xMxrgan@users.noreply.github.com>
2024-11-07 17:03:03 +0100

    Add files via upload

---
+++ Java ☕️/Past exams/Esame01/MultiWordIdentifier/src/SnakeFileIdentifier.java
@@ -0,0 +1,8 @@
+/*
+Si completi la classe concreta SnakeStyleIdentifier, sottoclasse di MultiWordIdentifier, analoga-
+mente a quanto fatto per CamelStyleIdentifier.
+ */
+
+public class SnakeFileIdentifier extends MultiWordIdentifier {
+
+}
