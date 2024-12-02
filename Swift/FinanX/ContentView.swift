d67160208153d6f1fe4535991fe134acd0638427
Morgan <104979421+xMxrgan@users.noreply.github.com>
2024-12-02 16:09:34 +0100

    Add files via upload

---
+++ Swift/FinanX/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  FinanX
+//
+//  Created by Morgan Picariello on 30/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+          Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            Image("Xlogo")
+
+          }
+                 )
+        }
+        .padding()
+        }
+    }
+
+#Preview {
+    ContentView()
+}
---
+++ Swift/FinanX/FinanXApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanXApp.swift
+//  FinanX
+//
+//  Created by Morgan Picariello on 30/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FinanXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
