7ae7c097b527ba1b2a1695d5e4615502c2224ffb
Morgan <104979421+xMxrgan@users.noreply.github.com>
2024-12-06 20:17:30 +0100

    Update ScientificCalc.java

--- Java ☕️/CalculatorApp/src/ScientificCalc.java
+++ Java ☕️/CalculatorApp/src/ScientificCalc.java
@@ -6,5 +6,18 @@ public class ScientificCalc {
         this.x = x;
         this.operand = operand;
         this.y = y;
+        result(this.x, this.operand, this.y);
+    }
+}
+
+public void result(double x, char operand, double y) {
+        switch (operand) {
+            case '+': System.out.println(x + y);
+            case '-': System.out.println(x - y);
+            case '*': System.out.println(x * y);
+            case '/': System.out.println(x / y);
+            case '%': System.out.println(x % y);
+            default: System.out.println("Something wrong");
+        }
     }
 }
