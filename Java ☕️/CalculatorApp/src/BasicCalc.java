2d8073746531bf4b0dfd4afa688166de6e617ac0
Morgan <104979421+xMxrgan@users.noreply.github.com>
2024-12-06 18:37:10 +0100

    Update BasicCalc.java

--- Java ☕️/CalculatorApp/src/BasicCalc.java
+++ Java ☕️/CalculatorApp/src/BasicCalc.java
@@ -1,20 +1,28 @@
 public class BasicCalc {
     private double x, y;
-    private char operation;
+    private char operand;
 
-    public BasicCalc (int x, char operation, int y) {
+    public BasicCalc (int x, char operand, int y) {
         this.x = x;
-        this.operation = operation;
+        this.operand = operand;
         this.y = y;
+        result(this.x, this.operand, this.y);
     }
     public BasicCalc (double x, char operation, double y) {
         this.x = x;
-        this.operation = operation;
+        this.operand = operation;
         this.y = y;
     }
 
-    void result(int x, int y, char str) {
-
+    public void result(double x, char operand, double y) {
+        switch (operand) {
+            case '+': System.out.println(x + y);
+            case '-': System.out.println(x - y);
+            case '*': System.out.println(x * y);
+            case '/': System.out.println(x / y);
+            case '%': System.out.println(x % y);
+            default: System.out.println("Something wrong");
+        }
     }
 
 }
